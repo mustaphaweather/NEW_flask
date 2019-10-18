1cd4bd01f14d93324bcba80b281e774994762067
mustaphaweather <chaklimustapha@gmail.com>
2019-10-18 16:46:41 +0100

    adding the models and path models and automatic model selection

--- src/services/model_wrapper.py
+++ src/services/model_wrapper.py
@@ -1,10 +1,22 @@
+# import pickle
+# import xgboost
 
-
-class Model_wrapper:
+class Model_wrapper():
 	## constructor
-	def __init__(self):
-		self.a = 2
+	def __init__(self, model_name):
+		self.model_name = model_name
+		self.mod_dict = {
+		'dpl': 'C:/Users/ASUS/Desktop/ML_API/NEW_flask/src/services/models/deep_learning.pkl' ,
+		'xgb': 'C:/Users/ASUS/Desktop/ML_API/NEW_flask/src/services/models/XGBRegressor.pkl',
+		'rnd': 'C:/Users/ASUS/Desktop/ML_API/NEW_flask/src/services/models/RandomForest.pkl'
+		}
+
+		try:
+			self.model_path = self.mod_dict.get(self.model_name)
+		except KeyError:
+			response = {} , 404
 
 	## predict function
 	def predict(self):
-		return self.a
+		# model = pickle.load(open(self.model_path))
+		return self.model_path
