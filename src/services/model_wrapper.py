28b872291387c3aea3da171e039fd533e59b101f
mustaphaweather <chaklimustapha@gmail.com>
2019-10-21 16:44:28 +0100

    adding the input samples conditions (positif and shape) & raising exception if model dosnt exist

--- src/services/model_wrapper.py
+++ src/services/model_wrapper.py
@@ -2,6 +2,7 @@ import pickle
 import xgboost
 import os
 import pandas as pd
+
 # import scikit-learn
 
 class Model_wrapper():
@@ -14,15 +15,14 @@ class Model_wrapper():
 		'rnd': 'C:/Users/ASUS/Desktop/ML_API/NEW_flask/src/services/models/RandomForest'
 		}
 
-		try:
-			self.model_path = self.mod_dict.get(self.model_name)
+		
+		self.model_path = self.mod_dict.get(self.model_name)
+		if self.model_path != None:
 			self.model = pickle.load(open(self.model_path, 'rb'))
-		except KeyError:
-			response = {} , 404
+		else:
+			raise Exception('this model name : {}'.format(self.model_name) + ' is not available')
 
 	## predict function
 	def predict(self ,arr):
-		# model = pickle.load(open(self.model_path))
 		S1_pd = pd.DataFrame(arr , columns = ['asset_1','asset_2','asset_3','asset_4','asset_5'])
-		print(self.model.predict(S1_pd))
 		return self.model.predict(S1_pd).tolist()
