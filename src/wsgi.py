ae2dbf7e8fa87a334d6345ab850284bceecbf50e
mustaphaweather <chaklimustapha@gmail.com>
2019-11-01 11:54:26 +0100

    adding gevent API OI asynchrouns

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,4 +1,13 @@
 from . import create_app
+from gevent.pywsgi import WSGIServer
+from config import Config
+
 
 
 application = create_app()
+print(Config.KEY_PATH)
+print(Config.CERT_PATH)
+server = WSGIServer(('127.0.0.1', 5000), application)
+# server = WSGIServer(('127.0.0.1', 5000 ), application,  keyfile=Config.KEY_PATH, certfile=Config.CERT_PATH)
+# server = WSGIServer(('127.0.0.1', 5000), application,  keyfile='key.pem' , certfile='cert.pem')
+server.serve_forever()
